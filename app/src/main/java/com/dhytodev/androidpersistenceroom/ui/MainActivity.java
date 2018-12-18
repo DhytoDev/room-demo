4bec3b50370d9cebc978a1cc38df76168c1689e6
Fordyta Abubakar <dhytodev@gmail.com>
2018-12-18 18:07:04 +0800

    add viewmodel factory

--- app/src/main/java/com/dhytodev/androidpersistenceroom/ui/MainActivity.java
+++ app/src/main/java/com/dhytodev/androidpersistenceroom/ui/MainActivity.java
@@ -1,37 +1,30 @@
 package com.dhytodev.androidpersistenceroom.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
 
 import com.dhytodev.androidpersistenceroom.R;
 import com.dhytodev.androidpersistenceroom.data.AppDatabase;
 import com.dhytodev.androidpersistenceroom.data.DatabaseInitializer;
-import com.dhytodev.androidpersistenceroom.data.User;
 
-import java.util.List;
-import java.util.Locale;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppDatabase mDb;
 
-    private TextView mYoungUsersTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mYoungUsersTextView = findViewById(R.id.young_users_tv);
+
+
 
         // Note: Db references should not be in an activity.
         mDb = AppDatabase.getInMemoryDatabase(getApplicationContext());
 
         populateDb();
-
-//        fetchData();
     }
 
     @Override
@@ -44,27 +37,4 @@ public class MainActivity extends AppCompatActivity {
         DatabaseInitializer.populateSync(mDb);
         DatabaseInitializer.addUser(mDb, "3", "Bambang");
     }
-
-    /*private void fetchData() {
-        // Note: this kind of logic should not be in an activity.
-        StringBuilder sb = new StringBuilder();
-        List<User> users = mDb.userModel().loadAllUsers();
-
-        for (User user : users) {
-            sb.append(String.format(Locale.US,
-                    "%s, %s \n", user.id, user.nama));
-        }
-        Log.d("users", sb.toString());
-
-        *//*for (int i = 0; i < users.size(); i++) {
-            sb.append(String.format(Locale.US,
-                    "%s, %s \n", users.get(i).id, users.get(i).nama));
-        }*//*
-
-        mYoungUsersTextView.setText(sb);
-
-       *//* User user = mDb.userModel().loadUserById("1");
-
-        mYoungUsersTextView.setText(user.nama);*//*
-    }*/
 }
--- app/src/main/java/com/dhytodev/androidpersistenceroom/ui/UserViewModel.java
+++ app/src/main/java/com/dhytodev/androidpersistenceroom/ui/UserViewModel.java
@@ -3,17 +3,33 @@ package com.dhytodev.androidpersistenceroom.ui;
 import com.dhytodev.androidpersistenceroom.data.AppDatabase;
 import com.dhytodev.androidpersistenceroom.data.User;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.paging.LivePagedListBuilder;
 import androidx.paging.PagedList;
 
 public class UserViewModel extends ViewModel {
-    private AppDatabase mDb ;
-    private LiveData<PagedList<User>> userList ;
+    private AppDatabase mDb;
+    private LiveData<PagedList<User>> userList;
 
     public UserViewModel(AppDatabase mDb) {
         this.mDb = mDb;
         userList = new LivePagedListBuilder<>(this.mDb.userModel().getAllUsers(), 5).build();
+    }
+
+    public static class Factory extends ViewModelProvider.NewInstanceFactory {
+        private final AppDatabase mDb;
+
+        public Factory(AppDatabase mDb) {
+            this.mDb = mDb;
+        }
+
+        @NonNull
+        @Override
+        public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+            return (T) new UserViewModel(mDb);
+        }
     }
 }
